347537606c1b7cc079bdc27cb0ca457f3c18e88a
CesarHdezAlvz <c.hdez.alvz@gmail.com>
2025-07-25 20:33:44 +0200

    Change in name of welcome page
    Ancient Welcome page was renamed and removed from the site

---
+++ src/components/GallerySelection.svelte
@@ -0,0 +1,22 @@
+<script>
+	import Selection from "./Selection.svelte";
+	import LangSelection from "./LangSelection.svelte";
+	import { currentLanguage } from '../stores/language.js';
+    import { getTranslation } from '../lib/translations.js';
+
+//	$: t = getTranslation($currentLanguage).welcome;
+</script>
+
+<!-- 
+
+<svelte:head>
+    <title>{t.title}</title>
+</svelte:head>
+
+<Selection
+	option1={t.portfolio}
+	option2={t.development}
+	option3={t.tutoring}
+/>
+
+-->
